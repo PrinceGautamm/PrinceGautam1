88b1faa77898489776ba36f4bbaeee5052029821
PrinceGautamm <91344201+PrinceGautamm@users.noreply.github.com>
2021-09-25 22:05:19 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const  mongoose  = require("mongoose");
+const path = require("path");
+const app = express();
+const bodyParser = require("body-parser");
+mongoose.connect('/mongodb://localhost/contact', {useNewUrlParser: true});
+const port = 8000;
+
+// Define mongoose Schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone : String,
+    email : String,
+    adress : String,
+    desc : String
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')); // For serving static files
+app.use(express.urlencoded());
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug'); // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const params = { };
+    res.status(200).render('home.pug', params);
+});
+
+app.get('/contact', (req, res)=>{
+    const params = { };
+    res.status(200).render('contact.pug', params);
+});
+
+app.post('/contact', (req, res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+    res.send("Submitted Successfully")
+    }).catch(()=>{
+    res.status(400).send("An error occured while submitting")
+})});
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
+
